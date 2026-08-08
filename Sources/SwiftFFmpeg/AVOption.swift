7fab99f2ebefdb2bdc12d6bb0843f6f53c398f37
Florian Zand <florianzand@me.com>
2026-08-08 06:41:28 +0200

    Update AVOption.swift

--- Sources/SwiftFFmpeg/AVOption.swift
+++ Sources/SwiftFFmpeg/AVOption.swift
@@ -49,7 +49,7 @@ public struct AVOption {
             case .flags:
                 self.min = nil
                 self.max = nil
-                self.defaultValue = AVOption.Flag(rawValue: Int32(clamping: native.default_val.i64))
+                self.defaultValue = AVOption.Flag(rawValue: Int32(native.default_val.i64))
             case .int:
                 self.min = Int32(clamping: native.min)
                 self.max = Int32(clamping: native.max)
