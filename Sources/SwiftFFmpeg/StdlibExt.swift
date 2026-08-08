24afda3813027b21c0f93cb17186563a6d740d61
Florian Zand <florianzand@me.com>
2026-08-08 13:56:49 +0200

    Update StdlibExt.swift

--- Sources/SwiftFFmpeg/StdlibExt.swift
+++ Sources/SwiftFFmpeg/StdlibExt.swift
@@ -42,12 +42,15 @@ extension String {
     self.init(cString: cString)
   }
     
+    /*
     init?(cString: UnsafeMutablePointer<CChar>?) {
       guard let cString = cString else {
         return nil
       }
+        UnsafePointer.init(cString)
       self.init(cString: cString)
     }
+    */
 }
 
 extension Dictionary where Key == String, Value == String {
